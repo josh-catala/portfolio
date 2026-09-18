358530c6f823f4529f5a91e299b24543366b1150
Joshua <jls.catala@gmail.com>
2026-09-18 02:26:44 -0600

    Fix import statement for createRoot to use 'react-dom/client'

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react';
+import { createRoot } from 'react-dom/client'; // Must be 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
